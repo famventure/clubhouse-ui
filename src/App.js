a2010d0dd0ced568dc22c4fe79ebca7c06bf40a2
Gordon Rich <gordon@famventure.com>
2023-02-23 16:48:01 -0800

    LP functional rework
    error free nested carousels (stateful) - wip.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
 import React, { useState, useEffect } from 'react';
+import tripData from "./stub_data.json";
 import './App.css';
 
 function handleClick(el) {
   el.classList.toggle('Active');
 }
 
-function Carousel(props) {
-  const children = props.label.slice();
+function Carousel(props, label) {
+  const children = props.slice();
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [length, setLength] = useState(children.length);
+
+  const next = () => {
+    if (currentIndex < (length - 1)) {
+      setCurrentIndex(prevState => prevState + 1)
+    }
+  }
+
+  const prev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(prevState => prevState - 1)
+    }
+  }
+
+  useEffect(() => {
+    setLength(children.length)
+  }, [children])
+
+  return (
+    <div className="carousel-container">
+      <div className="carousel-wrapper">
+        {currentIndex > 0 &&
+          <button onClick={prev} className="left-arrow">
+            {"("}
+          </button>}
+        <div className="carousel-content-wrapper">
+          <div className={label + " carousel-content"}>
+
+          {children.map(
+              item => (
+                <div key={item.id + " - " + item.label} className="Accordians" style={{ transform: `translateX(-${currentIndex * 103.5}%)` }} onClick={({ target }) => handleClick(target)}>
+                  {
+                    item.label
+                  }
+                </div>
+              )
+            )}
+
+          </div>
+        </div>
+        {currentIndex < (length - 1) &&
+          <button onClick={next} className="right-arrow">
+          {")"}
+          </button>}
+      </div>
+    </div>
+  );
+}
+
+function DestinationCarousel(props) {
+  const children = props.slice();
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const [length, setLength] = useState(children.length);
@@ -35,14 +89,18 @@ function Carousel(props) {
             &lt;
           </button>}
         <div className="carousel-content-wrapper">
-          <div className={props.category + " carousel-content"}>
-            {props.label.map(
-              x => (x.display.map(
-                item => (<div key={item + x} className="Accordians" style={{ transform: `translateX(-${currentIndex * 103.5}%)` }} onClick={({ target }) => handleClick(target)}>
-                  {item}
-                </div>)
-              ))
+          <div className={"Destination carousel-content"}>
+
+          {children.map(
+              item => (
+                <div key={item.id + item.country} className="DestAccordians Accordians" style={{ transform: `translateX(-${currentIndex * 103.5}%)` }} onClick={({ target }) => handleClick(target)}>
+                  {<p>{item.label}</p>}
+                  {<p>{item.description}</p>}
+                  {Carousel(item.acommodation, "Accomodation")}
+                </div>
+              )
             )}
+
           </div>
         </div>
         {currentIndex < (length - 1) &&
@@ -56,49 +114,6 @@ function Carousel(props) {
 
 function App() {
 
-  let destination = {
-    category: "Destination",
-    label:
-      [
-        {
-          display: ["Barcelona - Spain"],
-          accomodation: ["Cotton House Inn", "Hotel Barcelona Dos", "Hotel Barcelona 3"],
-        },
-        {
-          display: ["Lisbon - Portugal"],
-          accomodation: ["Portugal House Inn", "Hotel Portugal Dos"],
-        },
-        {
-          display: ["Casablanca - Morocco"],
-          accomodation: ["Casablanca House Inn", "Hotel Casablanca Dos", "Hotel Casablanca 3", "Casablanca 4 hotel"],
-        },
-      ]
-  };
-  let acommodation = {
-    category: "Accomodation",
-    label:
-      [
-        {
-          display: ["Cotton House Inn"],
-        },
-        {
-          display: ["Hotel Dos"],
-        },
-        {
-          display: ["Hotel 3"],
-        },
-      ]
-  };
-  let entertainment = {
-    category: "Entertainment",
-    label:
-      [
-        {
-          display: ["Sagrada Familia Guided Tour", "Mercado de la Boqueria", "Picasso Museum", "Spotify Camp Nou - FC Barcelona vs some other soccer team"],
-        },
-      ]
-  };
-
   return (
     <div className="App">
 
@@ -107,9 +122,7 @@ function App() {
       </header>
 
       <div>
-        {Carousel(destination)}
-        {Carousel(acommodation)}
-        {Carousel(entertainment)}
+        {DestinationCarousel(tripData)}
       </div>
 
       <footer className="App-footer">
