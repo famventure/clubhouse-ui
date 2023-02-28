3f0f8e0fbf22a90d0433950befd9faa17cb180e3
Gordon Rich <gordon@famventure.com>
2023-02-28 11:32:37 -0800

    Moving from hooks in functions to state updates in classes to support chained API loading
    refactored destination carousel loader and data fetcher to be a class instead of a function; the former would not allow getting hotel data dynamically based on a destination id.

--- src/App.js
+++ src/App.js
@@ -63,87 +63,111 @@ function Carousel(props, label) {
   );
 }
 
-function DestinationCarousel(props) {
+class DestCarousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      tripData: [],
+      destBoxExpanded: false,
+      currentIndex: 0,
+      length: 0,
+    };
+  }
 
-  const children = props.slice();
+  componentDidMount() {
+    fetch("https://xryp-e5xj-oseo.n7.xano.io/api:-VCn350s/destinations_list")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            tripData: result.slice(),
+            length: result.length,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  };
 
-  const [destBoxExpanded, setDestBoxExpanded] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [length, setLength] = useState(children.length);
-
-  const toggleDiv = (e) => {
-    setDestBoxExpanded(!destBoxExpanded);
+  toggleDiv = (e) => {
+    this.setState({
+      destBoxExpanded: !this.state.destBoxExpanded,
+    })
     e.stopPropagation();
   };
 
-  const next = (e) => {
-    if (currentIndex < (length - 1)) {
-      setCurrentIndex(prevState => (prevState + 1))
+  next = (e) => {
+    if (this.state.currentIndex < (this.state.length - 1)) {
+      this.setState({
+        currentIndex: this.state.currentIndex + 1,
+      })
     }
     e.stopPropagation();
   }
 
-  const prev = (e) => {
-    if (currentIndex > 0) {
-      setCurrentIndex(prevState => prevState - 1)
+  prev(e) {
+    if (this.state.currentIndex > 0) {
+      this.setState({
+        currentIndex: this.state.currentIndex - 1,
+      })
     }
     e.stopPropagation();
   }
 
-  useEffect(() => {
-    setLength(children.length)
-  }, [children])
-
-  return (
-    <div className="carousel-container">
-      <div className="carousel-wrapper">
-        {currentIndex > 0 &&
-          <button onClick={(e) => prev(e)} className="left-arrow">
-            &lt;
-          </button>}
-        <div className="carousel-content-wrapper">
-          <div className={"Destination carousel-content"}>
-
-            {children.map(
-              item => (
-                <div key={item.id} className={destBoxExpanded ? "Active DestAccordians Accordians" : "DestAccordians Accordians"} style={{ transform: `translateX(-${currentIndex * 103.5}%)` }} onClick={(e) => toggleDiv(e)}>
-                  {<div key={item.id + item.City} className="DestAccordiansHeader">{item.Name}</div>}
-                  {<div key={item.id + item.Neighborhood} className="DestAccordiansTagline">{item.Neighborhood}</div>}
-                  <ul>
-                    {Object.entries(item.bulletPoints).map(([key, value]) => (
-                      <li key={key}>"{value}"</li>
-                    ))}
-                  </ul>
-                  
-                </div>
-              )
-            )}
-
+  render() {
+    const { error, isLoaded, tripData } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="carousel-container">
+        <div className="carousel-wrapper">
+          {this.state.currentIndex > 0 &&
+            <button onClick={(e) => this.prev(e)} className="left-arrow">
+              &lt;
+            </button>}
+          <div className="carousel-content-wrapper">
+            <div className={"Destination carousel-content"}>
+  
+              {tripData.map(
+                item => (
+                  <div key={item.id} className={this.state.destBoxExpanded ? "Active DestAccordians Accordians" : "DestAccordians Accordians"} style={{ transform: `translateX(-${this.state.currentIndex * 103.5}%)` }} onClick={(e) => this.toggleDiv(e)}>
+                    {<div key={item.id + item.City} className="DestAccordiansHeader">{item.Name}</div>}
+                    {<div key={item.id + item.Neighborhood} className="DestAccordiansTagline">{item.Neighborhood}</div>}
+                    <ul>
+                      {Object.entries(item.bulletPoints).map(([key, value]) => (
+                        <li key={key}>"{value}"</li>
+                      ))}
+                    </ul>
+  
+                  </div>
+                )
+              )}
+  
+            </div>
           </div>
+          {this.state.currentIndex < (this.state.length - 1) &&
+            <button onClick={(e) => this.next(e)} className="right-arrow">
+              &gt;
+            </button>}
         </div>
-        {currentIndex < (length - 1) &&
-          <button onClick={(e) => next(e)} className="right-arrow">
-            &gt;
-          </button>}
       </div>
-    </div>
-  );
+      );
+    }
+  }
+
 }
 
 function App() {
-
-  const [tripData, setTripData] = useState([]);
-  useEffect(() => {
-    fetch("https://xryp-e5xj-oseo.n7.xano.io/api:-VCn350s/destinations_list")
-      .then((response) => response.json())
-      .then((data) => {
-        /*         console.log(data); */
-        setTripData(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
 
   return (
     <div className="App">
@@ -153,7 +177,7 @@ function App() {
       </header>
 
       <div>
-        {DestinationCarousel(tripData)}
+        <DestCarousel />
       </div>
 
       <footer className="App-footer">
